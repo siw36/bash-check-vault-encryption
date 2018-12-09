a321b3ee940c54f6cab14ad42a4bc7c4c2816cd3
siw36 <rhh.klussmann@gmail.com>
2018-12-09 21:00:44 +0100

    initial upload

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# bash-check-vault-encryption
-"Pre hook" for git push. Checks if every file that has "*vault*" in its name is encrypted before performing a git push.
+# Check vault encryption
+This script checks if every file which name contains vault is encrypted and gives you helpful output before pushing to a git repository.  
+
+
+
+## Usage
+__Manual invocation:__  
+Navigate to the git repository you want to push and exec the script.  
+
+__Automated invocation:__  
+Suitable if you want to perform this check every time you want to push new code to a git repository.  
+Set an alias in your `.bashrc` to call this script.  
+```bash
+alias safepush='<path to the script>check_vault.sh'
+```
+*Note: you can't set a "two word" alias.*  
+All arguments passed to the script are taken and used for the actual `git push` command.  
+Emaples:  
+```bash
+# No arguments
+safepush
+
+# Origin and branch name
+safepush origin master
+```
+Sample output:  
+![Image](https://github.com/siw36/bash-check-vault-encryption/blob/master/images/sample_output.png)
---
+++ check_vault.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Get all arguments that where passed to git push
+ARGUMENTS=`echo "$@"`
+
+# Preparing variables
+WD=`pwd`
+SEARCH='$ANSIBLE_VAULT;'
+FILES=`find -L $WD -iname '*vault*' -type f`
+SOMETHINGISWRONG=0
+NC='\033[0m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+
+# Check each file that contains vault in its name
+for i in $FILES ; do
+  # Ignore empty files
+  META=`file $i | awk '{print $2}'`
+  if [[ $META != "empty" ]]; then
+    # Check if first line contains "$ANSIBLE_VAULT;"
+    LINE=`head -n 1 $i`
+    if [[ $LINE == *"$ANSIBLE_VAULT;"* ]]; then
+      NAME=`echo $i | sed 's/.*\///'`
+      printf "${GREEN}$NAME is encrypted.${NC}\n"
+    else
+      NAME=`echo $i | sed 's/.*\///'`
+      printf "${RED}$NAME is unencrypted.${NC}\n"
+      SOMETHINGISWRONG=1
+    fi
+  fi
+done
+
+if [[ $SOMETHINGISWRONG -eq 1 ]]; then
+  echo "There where unencrypted vault files found."
+  while true; do
+    read -p "Do you wish to push anyways? (yY/nN) " answer
+    case $answer in
+        [yY]* ) git push $ARGUMENTS;break;;
+        [nN]* ) exit;;
+        * ) echo "Please answer yes (y/Y) or no (n/N).";;
+    esac
+  done
+fi
